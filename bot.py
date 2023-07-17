47b7cf25cd8eb77b9f2fe7cf8b1408573bd6ad22
meghost <meghost@MacBook-Air-meghost.local>
2023-07-17 20:22:08 +0300

    new

--- bot.py
+++ bot.py
@@ -3,13 +3,15 @@ import os
 import pickle
 import re
 import logging
-from telethon import TelegramClient, events
-from telethon.tl.types import InputMessagesFilterPhotos, MessageMediaPhoto, MessageMediaDocument
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-from aiogram.types import ParseMode
-from aiogram.utils import executor
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command
+from aiogram.dispatcher.filters.state import State, StatesGroup
 from dotenv import load_dotenv
+from telethon.sync import TelegramClient
+from telethon import events
+from telethon.tl.types import InputMessagesFilterPhotos, MessageMediaPhoto, MessageMediaDocument
 
 # Load environment variables from .env file
 load_dotenv()
@@ -21,7 +23,6 @@ logging.basicConfig(level=logging.INFO)
 api_id = os.getenv("API_ID")
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
-phone_number = os.getenv("PHONE_NUMBER")
 
 # Initialize Telegram client and bot
 client = TelegramClient('erwerber77', api_id, api_hash)
@@ -31,6 +32,11 @@ dp.middleware.setup(LoggingMiddleware())
 
 # Load channel data from pickle files
 new_link = os.getenv("NEW_LINK")
+
+
+class AuthState(StatesGroup):
+    waiting_for_phone_number = State()
+    waiting_for_code = State()
 
 
 def load_channels(file_name):
@@ -57,8 +63,8 @@ try:
 except FileNotFoundError:
     channel_mapping = {}
 
-# Utility functions
 
+# Utility functions
 
 def replace_link(text, new_link):
     url_pattern = re.compile(
@@ -72,8 +78,8 @@ def replace_at_word(text, new_word):
         return text
     return re.sub(r'@(\w+)', new_word, text)
 
-# Handler for sending media
 
+# Handler for sending media
 
 async def send_media(message, destination_channel_id):
     if message.media and isinstance(message.media, (MessageMediaPhoto, MessageMediaDocument)):
@@ -81,8 +87,8 @@ async def send_media(message, destination_channel_id):
     else:
         return await client.send_message(destination_channel_id, message.text)
 
-# Register event handlers
 
+# Register event handlers
 
 @client.on(events.NewMessage(chats=channels))
 async def my_event_handler(event):
@@ -118,13 +124,41 @@ async def album_event_handler(event):
     for destination_channel_id in destination_channels:
         await client.send_file(destination_channel_id, media_list, caption=updated_caption)
 
+
 # Command handlers
 
-
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    start_message = "Привет! Я бот для автопостинга с чужих каналов в ваш личный. Чтобы увидеть список команд, введите /help\n\n"
-    await message.reply(start_message)
+async def start(message: types.Message, state: FSMContext):
+    await message.reply("Для авторизации, пожалуйста, отправьте свой номер телефона", reply_markup=types.ReplyKeyboardRemove())
+    await AuthState.waiting_for_phone_number.set()
+
+
+@dp.message_handler(state=AuthState.waiting_for_phone_number)
+async def process_phone_number(message: types.Message, state: FSMContext):
+    phone_number = message.text
+
+    # Implement the logic to send SMS with the confirmation code to the provided phone number
+    # You may need to use third-party services for sending SMS, such as Twilio
+
+    await message.reply("Мы отправили вам SMS с кодом подтверждения. Пожалуйста, введите код из SMS", reply_markup=types.ReplyKeyboardRemove())
+    await AuthState.waiting_for_code.set()
+    await state.update_data(phone_number=phone_number)
+
+
+@dp.message_handler(state=AuthState.waiting_for_code)
+async def process_code(message: types.Message, state: FSMContext):
+    code = message.text
+
+    async with state.proxy() as data:
+        phone_number = data['phone_number']
+
+    # Implement the logic to verify the code that was sent to the user
+    # For example, you can use Telethon methods to sign in with the provided phone number and code
+
+    await client.sign_in(phone_number, code)
+
+    await message.reply("Вы успешно авторизованы! Теперь вы можете использовать бота.", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
 
 
 @dp.message_handler(commands=['help'])
@@ -145,180 +179,15 @@ async def help(message: types.Message):
     await message.reply(help_message)
 
 
-@dp.message_handler(commands=['add_channel'])
-async def add_channel(message: types.Message):
-    try:
-        channel_id = int(message.get_args())
-        channels.add(channel_id)
-        save_channels('channels.pickle', channels)
-        await message.reply(f"Канал {channel_id} добавлен")
-    except (ValueError, IndexError):
-        await message.reply("Пожалуйста, укажите корректный ID канала: /add_channel -1021331290")
+# Add the AuthState class to handle the state
 
-
-@dp.message_handler(commands=['remove_channel'])
-async def remove_channel(message: types.Message):
-    try:
-        channel_id = int(message.get_args())
-        channels.discard(channel_id)
-        save_channels('channels.pickle', channels)
-        await message.reply(f"Канал {channel_id} удален")
-    except (ValueError, IndexError):
-        await message.reply("Пожалуйста, укажите корректный ID канала: /remove_channel -100323213123890")
-
-
-@dp.message_handler(commands=['list_channels'])
-async def list_channels(message: types.Message):
-    if channels:
-        response = "Список каналов-источников:\n" + \
-            "\n".join([f"{channel}" for channel in channels])
-    else:
-        response = "Список каналов-источников пуст"
-    await message.reply(response)
-
-
-@dp.message_handler(commands=['add_destination_channel'])
-async def add_destination_channel(message: types.Message):
-    try:
-        channel_id = int(message.get_args())
-        destination_channels.add(channel_id)
-        save_channels('destination_channels.pickle', destination_channels)
-        await message.reply(f"Канал-получатель {channel_id} добавлен")
-    except (ValueError, IndexError):
-        await message.reply("Пожалуйста, укажите корректный ID канала-получателя: /add_destination_channel -100321312890")
-
-
-@dp.message_handler(commands=['remove_destination_channel'])
-async def remove_destination_channel(message: types.Message):
-    try:
-        channel_id = int(message.get_args())
-        destination_channels.discard(channel_id)
-        save_channels('destination_channels.pickle', destination_channels)
-        await message.reply(f"Канал-получатель {channel_id} удален")
-    except (ValueError, IndexError):
-        await message.reply("Пожалуйста, укажите корректный ID канала-получателя: /remove_destination_channel -10013213127890")
-
-
-@dp.message_handler(commands=['list_destination_channels'])
-async def list_destination_channels(message: types.Message):
-    if destination_channels:
-        response = "Список каналов-получателей:\n" + \
-            "\n".join([f"{channel}" for channel in destination_channels])
-    else:
-        response = "Список каналов-получателей пуст"
-    await message.reply(response)
-
-
-@dp.message_handler(commands=['set_channel_mapping'])
-async def set_channel_mapping(message: types.Message):
-    args = message.get_args().split()
-    if len(args) != 2:
-        await message.reply(
-            "Пожалуйста, укажите ID канала-источника и ID канала-получателя через пробел: /set_channel_mapping -1001234327890 -10003219876324321"
-        )
-        return
-
-    try:
-        source_channel_id = int(args[0])
-        destination_channel_id = int(args[1])
-
-        if source_channel_id not in channels:
-            await message.reply(f"Канал-источник {source_channel_id} не найден в списке источников")
-            return
-
-        if destination_channel_id not in destination_channels:
-            await message.reply(f"Канал-получатель {destination_channel_id} не найден в списке получателей")
-            return
-
-        channel_mapping[source_channel_id] = destination_channel_id
-        save_channels('channel_mapping.pickle', channel_mapping)
-        await message.reply(f"Канал {source_channel_id} теперь будет пересылать контент на канал {destination_channel_id}")
-
-    except (ValueError, IndexError):
-        await message.reply("Пожалуйста, укажите корректные ID каналов: /set_channel_mapping -103213890 -100312312321")
-
-
-@dp.message_handler(commands=['last_messages'])
-async def send_last_messages_handler(message: types.Message):
-    args = message.get_args().split()
-    channel_id = None
-    limit = 1
-
-    if len(args) == 2:
-        try:
-            channel_id = int(args[0])
-            if args[1].lower() == "all":
-                limit = None
-            else:
-                limit = int(args[1])
-        except ValueError:
-            await message.reply("Пожалуйста, укажите корректные ID канала и количество сообщений: /last_messages -1001323290 5 или /last_messages -100321327832190 all")
-            return
-    elif len(args) == 1:
-        try:
-            if args[0].lower() == "all":
-                limit = None
-            else:
-                limit = int(args[0])
-        except ValueError:
-            await message.reply("Пожалуйста, укажите корректное количество сообщений: /last_messages 5 или /last_messages all")
-            return
-
-    await send_last_messages(channel_id, limit)
-    if limit is None:
-        await message.reply("Все сообщения отправлены!")
-    else:
-        await message.reply(f"{limit} последних сообщений отправлены!")
-
-
-async def send_last_messages(channel_id=None, limit=None):
-    if channel_id is not None:
-        if channel_id in channels:
-            chat = await client.get_entity(channel_id)
-            messages = await client.get_messages(chat, limit=limit)
-        else:
-            return
-    else:
-        messages = []
-        for channel_id in channels:
-            chat = await client.get_entity(channel_id)
-            channel_messages = await client.get_messages(chat, limit=limit)
-            messages.extend(channel_messages)
-
-    messages = sorted(messages, key=lambda x: x.date)
-
-    grouped_messages = {}
-    for message in messages:
-        if message.action is None:
-            if message.grouped_id:
-                if message.grouped_id not in grouped_messages:
-                    grouped_messages[message.grouped_id] = [message]
-                else:
-                    grouped_messages[message.grouped_id].append(message)
-            else:
-                grouped_messages[message.id] = [message]
-
-    for destination_channel_id in destination_channels:
-        for message_group in grouped_messages.values():
-            if len(message_group) > 1 and message_group[0].grouped_id:
-                media_list = [msg.media for msg in message_group]
-                caption = "\n".join([replace_link(replace_at_word(
-                    msg.text, "@test"), new_link) for msg in message_group if msg.text])
-                await client.send_file(destination_channel_id, media_list, caption=caption)
-            else:
-                if message_group[0].media:
-                    updated_text = replace_link(replace_at_word(
-                        message_group[0].text, "@test"), new_link)
-                    await client.send_file(destination_channel_id, message_group[0].media, caption=updated_text)
-                else:
-                    updated_text = replace_link(replace_at_word(
-                        message_group[0].text, "@test"), new_link)
-                    if updated_text:
-                        await client.send_message(destination_channel_id, updated_text)
+class AuthState(StatesGroup):
+    waiting_for_phone_number = State()
+    waiting_for_code = State()
 
 
 async def main():
-    await client.start(phone=phone_number)
+    await client.start()
     await client.connect()
 
     dp.register_message_handler(start, commands=['start'], commands_prefix='/')
@@ -326,6 +195,7 @@ async def main():
 
     await dp.start_polling()
     await client.run_until_disconnected()
+
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(main())
